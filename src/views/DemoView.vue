d807eec049d537a8304cbf322185392282827f99
Andréas B <andreas.blondeau@gmail.com>
2022-10-11 17:02:34 +0200

    ✨  Setup demo component

---
+++ src/views/DemoView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="demo">Bonjour</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.demo {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
