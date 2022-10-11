15647321f6df8ec14122b35ba36f634e3ffefac4
Andréas B <andreas.blondeau@gmail.com>
2022-10-11 16:46:14 +0200

    👋 Hello world!

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">Figma: In the file</h1>
+    <h3>
+      Let's discover tips to better collaborate between designers and engineers.
+    </h3>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
